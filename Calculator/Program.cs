f037d6364d9675a1e6752c157ff23a166665199b
axpalx <axpalx@gmail.com>
2022-03-28 21:18:30 -0300

    Calculadora Csharp

---
+++ Calculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Soma();
+      Subtracao();
+    }
+
+    static void Soma()
+    {
+      Console.Clear();
+      Console.WriteLine("Primeiro valor da soma: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo valor da soma: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 + v2;
+      Console.WriteLine("O resultado da soma é " + resultado);
+      Console.WriteLine($"O resultado da soma é {resultado}");
+      Console.WriteLine($"O resultado da soma é {v1 + v2}");
+      Console.ReadKey();
+    }
+
+    static void Subtracao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Primeiro valor da subtração: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo valor da subtração: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      float resultado = v1 - v2;
+
+      Console.WriteLine($"O resultado da subtração é {resultado}");
+      Console.ReadKey();
+    }
+  }
+
+
+}
